a5e963cff1fb0b3e039cd4240e58c5cdbf37aa87
Facundo M. Cabrera <cabre94@hotmail.com>
2025-05-09 12:19:24 -0300

    Add Key.h

---
+++ Cabrium/src/Cabrium/Common/Key.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cstdint>
+
+namespace cabrium {
+
+namespace key {
+
+enum KeyCode : uint16_t {
+    Key0 = 0,
+    Key1 = 1,
+    Key2 = 2,
+    Key3 = 3,
+};
+
+} // namespace key
+
+} // namespace cabrium
