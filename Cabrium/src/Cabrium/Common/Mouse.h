a3c8761fd173a0c8ef08b0ffda9def436a243ba5
Facundo M. Cabrera <cabre94@hotmail.com>
2025-05-09 12:16:02 -0300

    add Mouse.h

---
+++ Cabrium/src/Cabrium/Common/Mouse.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cstdint>
+
+namespace cabrium {
+
+namespace mouse {
+
+enum ButtonCode : uint16_t {
+    Button0 = 0,
+    Button1 = 1,
+    Button2 = 2,
+    Button3 = 3,
+};
+
+} // namespace mouse
+
+} // namespace cabrium
