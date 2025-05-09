a8f9d1510e88fdf0bcba612d7708e21671b7a2e6
Facundo M. Cabrera <cabre94@hotmail.com>
2025-05-09 09:06:33 -0300

    add events

---
+++ Cabrium/src/Cabrium/Events/ApplicationEvent.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "Cabrium/Events/Event.h"
+
+#include <cstdint>
+#include <sstream>
+
+namespace cabrium {
+
+class WindowResizeEvent : public Event {
+public:
+    WindowResizeEvent(uint32_t width_, uint32_t height_) : width(width_), height(height_) {}
+
+    uint32_t GetWidth() const { return width; }
+    uint32_t GetHeight() const { return height; }
+
+    std::string ToString() const override {
+        std::stringstream ss;
+        ss << "WindowResizeEvent: " << width << ", " << height;
+        return ss.str();
+    }
+
+    DECL_EVENT_CLASS_TYPE(WindowResize)
+    DECL_EVENT_CLASS_CATEGORY(EventCategory::EventCategoryApplication)
+
+private:
+    uint32_t width, height;
+};
+
+} // namespace cabrium
---
+++ Cabrium/src/Cabrium/Events/Event.h
@@ -0,0 +1,89 @@
+#pragma once
+
+#include "Cabrium/Core.h"
+
+#include <cstdint>
+#include <string>
+
+namespace cabrium {
+
+enum class EventType {
+    None = 0,
+
+    // Window events
+    WindowClose,
+    WindowResize,
+    WindowFocus,
+    WindowLostFocus,
+    WindowMoved,
+
+    // App events
+    AppTick,
+    AppUpdate,
+    AppRender,
+
+    // Keyboard events
+    KeyPressed,
+    KeyReleased,
+    KeyTyped,
+
+    // Mouse events
+    MouseButtonPressed,
+    MouseButtonRealesed,
+    MouseMoved,
+    MouseScrolled,
+};
+
+enum EventCategory : uint32_t {
+    None = 0,
+    EventCategoryApplication = BIT(0),
+    EventCategoryInput = BIT(1),
+    EventCategoryKeyboard = BIT(2),
+    EventCategoryMouse = BIT(3),
+    EventCategoryMouseButton = BIT(4)
+};
+
+// inline EventCategory operator|(EventCategory lhs, EventCategory rhs) {
+//     return static_cast<EventCategory>(static_cast<EventCategory>(lhs) |
+//                                       static_cast<EventCategory>(rhs));
+// }
+//
+// inline EventCategory operator&(EventCategory lhs, EventCategory rhs) {
+//     return static_cast<EventCategory>(static_cast<EventCategory>(lhs) &
+//                                       static_cast<EventCategory>(rhs));
+// }
+//
+// inline bool operator!(EventCategory category) {
+//     return static_cast<std::underlying_type_t<EventCategory>>(category) == 0;
+// }
+
+#define DECL_EVENT_CLASS_TYPE(type)                                             \
+    static EventType GetStaticType() { return EventType::type; }                \
+    virtual EventType GetEventType() const override { return GetStaticType(); } \
+    virtual const char *GetName() const override { return #type; }
+
+#define DECL_EVENT_CLASS_CATEGORY(category) \
+    virtual uint32_t GetCategoryFlags() const override { return static_cast<uint32_t>(category); }
+
+class Event {
+public:
+    virtual ~Event() {}
+
+    virtual EventType GetEventType() const = 0;
+    virtual const char *GetName() const = 0;
+    virtual uint32_t GetCategoryFlags() const = 0;
+    virtual std::string ToString() const { return GetName(); };
+
+    bool IsInCategory(EventCategory category) const {
+        return GetCategoryFlags() & static_cast<uint32_t>(category);
+    }
+
+private:
+    bool handled = false;
+};
+
+inline std::ostream &operator<<(std::ostream &os, const Event &e) { return os << e.ToString(); }
+
+inline std::string format_as(const Event &e) { return e.ToString(); }
+
+} // namespace cabrium
