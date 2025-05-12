20b564ff3bccdfe83d639a13e7f8f62244866c43
Facundo M. Cabrera <cabre94@hotmail.com>
2025-05-12 13:02:01 -0300

    update clang format

--- Cabrium/src/Cabrium/Application.cpp
+++ Cabrium/src/Cabrium/Application.cpp
@@ -2,22 +2,31 @@
 
 #include "Application.h"
 
+#include "Common/Window.h"
+
+#include <GLFW/glfw3.h>
+
 namespace cabrium {
 
-Application::Application() {}
+Application::Application() {
+    // window = std::make_unique<Window>;
+    window = std::unique_ptr<Window>(Window::create());
+}
 
 Application::~Application() {}
 
 void Application::run() {
 
-    // WindowResizeEvent e(1280, 720);
+    // while (getchar() != 'q')
+    //     ;
 
-    // if (e.isInCategory(EventCategoryApplication)) {
-    //     CBRM_TRACE(e);
-    // }
+    while (running) {
 
-    while (getchar() != 'q')
-        ;
+        glClearColor(0, 1, 1, 1);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        window->update();
+    }
 }
 
 } // namespace cabrium
