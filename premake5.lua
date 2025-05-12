af410c4247729d64d68ca4fa4b359e8d09d046b6
Facundo M. Cabrera <cabre94@hotmail.com>
2025-05-12 13:01:47 -0300

    Fix build on premake

--- premake5.lua
+++ premake5.lua
@@ -22,6 +22,14 @@ project "Cabrium"
 	kind "SharedLib"
 	language "C++"
 
+	-- for to use /MDd in premake: 
+	-- staticruntime "off"
+	-- runtime "Debug"
+	-- /MTd in premake:
+	-- staticruntime "on"
+	-- runtime "Debug"
+	staticruntime "on"
+
 	buildoptions { "/utf-8" }
 
 	targetdir  ("bin/" .. outputdir .. "/%{prj.name}")
@@ -84,10 +92,12 @@ project "Sandbox"
 	kind "ConsoleApp"
 	language "C++"
 
+	staticruntime "on"
+
 	buildoptions { "/utf-8" }
 
 	targetdir  ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir  ("bin-int/" .. outputdir .. "/%{prj.name}")
+	objdir  ("build/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
